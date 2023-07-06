466a4fc9d919ca2a147e7c995a25529fe64fb06d
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-07-06 15:27:49 +0500

    Change App component into class base component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import React, { Component } from "react";
 import Header from './components/Header';
 import NotesList from './components/NotesList'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NotesList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: "Search"
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <NotesList />
+      </div>
+    );
+  }
 }
 
 export default App;
