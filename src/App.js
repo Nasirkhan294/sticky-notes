0135bd49cda37bc69ae1cc99950ea12b0aa26bf9
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-07-08 15:00:48 +0500

    Add search note functionality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         doesMatchSearch: true
       },
     ],
-    searchText: "Search"
+    searchText: ""
   };
 
   addNote = () => {
@@ -41,13 +41,37 @@ class App extends Component {
         }
       }
     })
-    this.setState({notes: updatedNotes})
+    this.setState({ notes: updatedNotes })
+  }
+
+  onSearch = (text) => {
+    const newSearchText = text.toLowerCase();
+    const updatedNotes = this.state.notes.map((note) => {
+      if (!newSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(newSearchText);
+        const descriptionMatch = description.includes(newSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    })
+
+    this.setState({
+      notes: updatedNotes,
+      searchText: newSearchText
+    })
   }
 
   render() {
     return (
       <div>
         <Header
+          onSearch={this.onSearch}
           searchText={this.state.searchText}
           addNote={this.addNote}
         />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Header = (props) => {
+	const callSearch = (e) => {
+		props.onSearch(e.target.value);
+	}
 	return (
 		<header>
 			<h1 className="app-header__title">Super Sticky Notes</h1>
@@ -11,6 +14,7 @@ const Header = (props) => {
 					className="search"
 					placeholder="Type here to search..."
 					value={props.searchText}
+					onChange={callSearch}
 				/>
 			</aside>
 		</header>
