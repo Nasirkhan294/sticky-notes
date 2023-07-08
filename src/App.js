ec86328be46f47cd954c9cf6b7300da94b5f24fa
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-07-08 13:40:27 +0500

    Add new note to the notes array

--- src/App.js
+++ src/App.js
@@ -6,31 +6,33 @@ class App extends Component {
   state = {
     notes: [
       {
-        id: 0,
-        title: "eat",
-        description: "reese peanut butter cups",
+        id: Date.now(),
+        title: "",
+        description: "",
         doesMatchSearch: true
       },
-      {
-        id: 1,
-        title: "sleep",
-        description: "eight hours",
-        doesMatchSearch: false
-      },
-      {
-        id: 2,
-        title: "code",
-        description: "build an awesome ui",
-        doesMatchSearch: true
-      }
     ],
     searchText: "Search"
   };
 
+  addNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true
+    };
+
+    this.setState({ notes: [newNote, ...this.state.notes] });
+  }
+
   render() {
     return (
       <div>
-        <Header searchText={this.state.searchText} />
+        <Header
+          searchText={this.state.searchText}
+          addNote={this.addNote}
+        />
         <NotesList notes={this.state.notes} />
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
 		<header>
 			<h1 className="app-header__title">Super Sticky Notes</h1>
 			<aside className="app-header__controls">
-				<button className="add-new">+ New Note</button>
+				<button onClick={props.addNote} className="add-new">+ New Note</button>
 				<input
 					type="text"
 					className="search"
